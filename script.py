5b36b908bbfac955c3cb17cd987fa2aeb6c69f22
Chris Dryden <christopher.paul.dryden@gmail.com>
2018-01-28 22:22:33 -0500

    Added functionality for placement areas
    arrays to test out board configurations is added

--- script.py
+++ script.py
@@ -5,20 +5,67 @@ Created on Tue Jan 23 21:43:14 2018
 @author: Chris Dryden
 """
 import re
+import numpy as np
+
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+
+#
+#Sidenote corner case: if a word can be extended by another word and then another word is made off of that
+#
 
 
 
 
-pointsmatch
 
 
-
-
-endstring = "\n"
-letters = "wi\n"
-containedletters = "no"
+endstring = ""
+letters = "smaoeu"
+containedletters = "o"
 finalword = []
 longest = 0
+
+
+def board():
+    board = np.array([
+            ["~","~","~","~","~","~","A","~","~","~","~","~","~","~","~"],#1
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#2
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#3
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#4
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#5
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#6
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#7
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#8
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#9
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#10
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#11
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#12
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#13
+            ["~","~","~","~","~","~","~","~","~","~","~","~","~","~","~"],#14
+            ["~","~","~","~","~","~","~","~","~","~","~","~","A","~","~"],#15
+            ])
+    basewords = determineBaseWords(board)
+    print(basewords)
+    print(board.shape)
+
+
+def determineBaseWords(board):
+    for i in range(15):
+        for j in range(15):
+            print(board[i,j])
+    
+
+
+
+def pointsmatch(word):
+    totalscore = 0
+    for i in list(word):
+        totalscore = totalscore +  score[i]
+    return totalscore
+
 
 def validword(string, word):
     letters = list(string)
@@ -32,38 +79,28 @@ def validword(string, word):
 
 def scrabblesearch():
     f = open('words_alpha.txt', 'r')
-    longest = 0
-    longestword = []
+    highestpoints = 0
+    bestword = []
     searchlist = []
     for word in f:
         if containedletters in word:
             searchlist.append(word)
+    searchlist = [word[:-1] for word in searchlist]
     print(searchlist)
     for word in searchlist:
         if(validword(letters+containedletters, word)):
             print(word)
-            length = len(word)
-            print(length)
-            if length == longest:
-                longestword.append(word)
-            if length > longest:
-                longestword = []
-                longestword.append(word)
-                longest = length
-    print("longest word is " + str(longestword))
-        
+            points = pointsmatch(word)
+            if points == highestpoints:
+                bestword.append(word)
+            if points > highestpoints:
+                bestword = []
+                bestword.append(word)
+                highestpoints = points
+    print("longest word is " + str(bestword))
+    
+    
     
         
-    
-def oldsearch():
-    for word in f:
-        if re.match("^[abcdef]*$", word):
-            length = len(word)
-            if length == longest:
-                longestword.append(word)
-            if length > longest:
-                longestword = []
-                longestword.append(word)
-                longest = length
-scrabblesearch()
-
+#scrabblesearch()
+board()
