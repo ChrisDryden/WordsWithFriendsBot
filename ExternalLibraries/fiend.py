bfb62dfea1ed9389c875a6876c49022f899c7a24
Chris Dryden <christopher.paul.dryden@gmail.com>
2018-07-05 23:12:28 -0400

    removed outdated files

--- ExternalLibraries/fiend.py
+++ ExternalLibraries/fiend.py
@@ -1,19 +1,3 @@
-# Fiend - A Python module for accessing Zynga's Words with Friends
-# Copyright (C) 2011 Jahn Veach <j@hnvea.ch>
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 VERSION = '0.3'
 
 import base64
@@ -96,29 +80,6 @@ BONUS_SQUARES = [['-', '-', '-', '$', '-', '-', '!', '-', '!', '-', '-', '$', '-
                  ['-', '-', '+', '-', '-', '=', '-', '-', '-', '=', '-', '-', '+', '-', '-'],
                  ['-', '-', '-', '$', '-', '-', '!', '-', '!', '-', '-', '$', '-', '-', '-']]
 
-# Fiend - A Python module for accessing Zynga's Words with Friends
-# Copyright (C) 2011 Jahn Veach <j@hnvea.ch>
-#
-# This file is originally based on work by Sean Perry <shalehperry@attbi.com>.
-# Source: http://mail.python.org/pipermail/python-list/2002-October/775128.html
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# This is a pure Python implementation of the Mersenne Twister,
-# which Words with Friends uses as its random number generator.
-#
-# Mersenne Twister reference: http://en.wikipedia.org/wiki/Mersenne_twister
 
 N = 624
 M = 397
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import client_wwf as WWF
 from ExternalLibraries.fiend import Fiend
 
-username = 'christopher.paul.dryden@gmail.com'
-password = 'toronto9'
+username = 'redacted'
+password = 'redacted'
 
 def initial_main():
 	session = WWF.login(username, password)
@@ -16,13 +16,19 @@ def initial_main():
 
 def fiend_main():
 	game = Fiend(username, password)
-	print(game.games)
+	#print(game.games)
 	active_games = game.activeGames
 
 	for gameID in active_games:
-		print(active_games[gameID])
-		print(active_games[gameID].letterBagCodes)
-		print(active_games[gameID].remainingLetters)
+		#print(active_games[gameID])
+		#print(active_games[gameID].letterBagCodes)
+		#print(active_games[gameID].remainingLetters)
+		#print(active_games[gameID].remainingLetters)
+		print('game:')
+		print(active_games[gameID].opponent.rackLetters)
+		print(active_games[gameID].creator.rackLetters)
+
+
 
 
 fiend_main()
